c6da3abcc859bfa970ed553808d777275ea1d70d
Godoyysz <jgfuso@gmail.com>
2024-03-20 17:20:03 -0400

    ptac aula 20/03

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,21 @@ import Link from "next/Link";
 import Image from "next/image";
 
 export default async function Home() {
-  const resposta = await fetch("http://localhost:3000/api", {next: {rivalidate: 1}}); 
-  const campus = await resposta.json();
+  const resposta = await fetch("https://back-end-ifms.vercel.app/campi", {next: {rivalidate: 1}}); 
+  const campi = await resposta.json();
   
-  <Link href="https://ead.ifms.edu.br"><Image width={323}
-             height={100} 
-            src={"https://www.ifms.edu.br/imagens/imagens-noticias/abraco-pela-educacao/img_5497.jpg"}/></Link>
+  <Link href="https://ead.ifms.edu.br">
+    <Image width={323} height={100} src={"https://www.ifms.edu.br/imagens/imagens-noticias/abraco-pela-educacao/img_5497.jpg"}/>
+    </Link>
 
   return (
     <main>
       <h1>Home</h1>
       {
-        campus.map( (campi)=>
+        campi.map( (campus)=>
           <div>
-            <p>{campi.nome_campi}</p>
-            <img className="imgstyle" src={campi.imagem_url}/>
+            <p>{campus.nome_campus}</p>
+            <img className="imgstyle" src={campus.image_url}/>
           </div>
         )
       }
